cbcf48f220d04c96d321641e055401e0da72356b
imcoco18 <coco.jasmine18@icloud.com>
2020-05-02 23:00:34 +0900

    [Finish]linkエラー、グリッドシステム未

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
 
   root 'home#top'
-  get 'home/about'
-
-  resources :users,only: [:show,:index,:edit,:update]
+  get 'home/about' => 'home#about'
   resources :books
+  resources :users,only: [:show,:index,:edit,:update]
 end
